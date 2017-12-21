5077dad382bc2510d86331126584fcba8aab618d
Jason Vance <jbvance@gmail.com>
2017-12-21 02:39:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,10 @@
-let chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+let chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 const addElementToBeginningOfArray = (arr, el) => {
   return [el, ...arr];
-}
+};
 
 const destructivelyAddElementToBeginningOfArray = (arr, el) => {
   arr = [el, ...arr];
   return arr;
-}
-
+};
